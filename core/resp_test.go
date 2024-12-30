2901313f38339492a0b2f7743d668f664f91e033
Amrish <erpnext@LPT44-MP284NGJ-AMRISH>
2024-12-30 14:50:30 +0530

    Fix the testcase

--- core/resp_test.go
+++ core/resp_test.go
@@ -1,8 +1,8 @@
 package core_test
 
 import (
-	"fmt"
 	"log"
+	"reflect"
 	"testing"
 
 	"github.com/amrishkshah/dicedb/core"
@@ -13,9 +13,14 @@ func TestSimpleStringDecode(t *testing.T) {
 		"+OK\r\n": "OK",
 	}
 	for k, v := range cases {
-		value, _ := core.Decode([]byte(k))
-		if v != value {
-			t.Fail()
+		values, _ := core.Decode([]byte(k))
+		println(v)
+		println("Test ---->")
+		//log.Panicln(values)
+		for _, value := range values {
+			if v != value {
+				t.Fail()
+			}
 		}
 	}
 }
@@ -25,9 +30,11 @@ func TestErrorDecode(t *testing.T) {
 		"-Error Message\r\n": "Error Message",
 	}
 	for k, v := range cases {
-		value, _ := core.Decode([]byte(k))
-		if v != value {
-			t.Fail()
+		values, _ := core.Decode([]byte(k))
+		for _, value := range values {
+			if v != value {
+				t.Fail()
+			}
 		}
 	}
 }
@@ -37,9 +44,11 @@ func TestInt64Decode(t *testing.T) {
 		":123\r\n": 123,
 	}
 	for k, v := range cases {
-		value, _ := core.Decode([]byte(k))
-		if v != value {
-			t.Fail()
+		values, _ := core.Decode([]byte(k))
+		for _, value := range values {
+			if v != value {
+				t.Fail()
+			}
 		}
 	}
 }
@@ -50,10 +59,12 @@ func TestBulkStringDecode(t *testing.T) {
 		"$0\r\n\r\n":      "",
 	}
 	for k, v := range cases {
-		value, _ := core.Decode([]byte(k))
-		log.Println(value)
-		if v != value {
-			t.Fail()
+		values, _ := core.Decode([]byte(k))
+		log.Println(values)
+		for _, value := range values {
+			if v != value {
+				t.Fail()
+			}
 		}
 	}
 }
@@ -64,17 +75,31 @@ func TestArrayDecode(t *testing.T) {
 		"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n":                     {"hello", "world"},
 		"*3\r\n:1\r\n:2\r\n:3\r\n":                                 {int64(1), int64(2), int64(3)},
 		"*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n":            {int64(1), int64(2), int64(3), int64(4), "hello"},
-		"*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n": {[]int64{int64(1), int64(2), int64(3)}, []interface{}{"Hello", "World"}},
+		"*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n": {[]interface{}{int64(1), int64(2), int64(3)}, []interface{}{"Hello", "World"}},
 	}
-	for k, v := range cases {
-		value, _ := core.Decode([]byte(k))
-		array := value.([]interface{})
-		if len(array) != len(v) {
-			t.Fail()
+
+	for input, expected := range cases {
+		values, err := core.Decode([]byte(input))
+		if err != nil {
+			t.Errorf("Decode(%q) returned error: %v", input, err)
+			continue
 		}
-		for i := range array {
-			if fmt.Sprintf("%v", v[i]) != fmt.Sprintf("%v", array[i]) {
-				t.Fail()
+
+		// array, ok := values.([]interface{})
+		// if !ok {
+		// 	t.Errorf("Decode(%q) returned %T; want []interface{}", input, values)
+		// 	continue
+		// }
+		arr := values[0].([]interface{})
+		//log.Panicln()
+		if len(arr) != len(expected) {
+			t.Errorf("Decode(%q) returned array of length %d; want %d", input, len(arr), len(expected))
+			continue
+		}
+
+		for i := range arr {
+			if !reflect.DeepEqual(expected[i], arr[i]) {
+				t.Errorf("Decode(%q) element %d = %v; want %v", input, i, arr[i], expected[i])
 			}
 		}
 	}
